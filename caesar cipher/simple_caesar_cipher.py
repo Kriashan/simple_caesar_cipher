6a867f763efadb20b7dc5776c714864dbf0ced87
Kriashan <es19090@melville-high.school.nz>
2022-10-31 14:33:30 +1300

    Made the cipher fully functional

--- caesar cipher/simple_caesar_cipher.py
+++ caesar cipher/simple_caesar_cipher.py
@@ -1,3 +1,7 @@
+import string
+import sys
+import re
+
 #review time: 1667614000.8258696
 def self_expire(times_review_per_year=40):
     import time
@@ -38,7 +42,53 @@ def self_expire(times_review_per_year=40):
                 previous_line = line
     review_file(get_lines(get_line_count()))
 
+# Asks the user a question until answer is not blank and is expected type
+#review time: 1668091889.6146588
+def ask_user_question(ask='', re_filter='', expected_type='str'):
+    if (answer := ''.join(re.findall(re_filter, input(ask)))) != '': 
+        if expected_type == 'str':
+            answer = str(answer)
+            return answer
+        elif expected_type == 'int':
+            answer = int(answer)
+            return answer
+        elif expected_type == 'float':
+            answer = float(answer)
+            return answer
+        else:
+            stdout('Unknown type expected {0}, options are str, int, and float'.format(expected_type))
+            return None
+    else:
+        stdout('Input cannot be blank and must be type {0}'.format(expected_type))
+        stdout('Input is: {0}'.format(answer))
+    return ask_user_question(ask, re_filter, expected_type)
+
+#review time: 1667968082.8530009
+def cipher():
+    letters = list(string.ascii_letters + string.digits + ' ' + string.punctuation)
+    print(letters)
+
+    encode = input("Enter your string to encode: ")
+    key = ''.join(re.findall('[0-9-]', input("How much do you want to shift this by: ")))
+    while key.__contains__('--'):
+        key = key.replace('--', '-')
+    key = int(key)
+    new_string = []
+
+    for letter in encode:
+        print((letters.index(letter) + key) % len(letters))
+        new_string.append(letters[(letters.index(letter) + key) % len(letters)])
+
+    print(new_string)
+
+# Adds \n and uses the faster version of print
+#review time: 1668091889.6146588
+def stdout(text):
+    sys.stdout.write(str(text) + '\n')
+
 if __name__ == '__main__':
     self_expire()
+    stdout('This program is a caesar cipher')
+    cipher()
 else:
     self_expire()
