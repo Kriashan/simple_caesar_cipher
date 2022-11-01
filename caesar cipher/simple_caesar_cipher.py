e464a6df626292f6877db16b4c7f232d3eff0ad2
Kriashan <es19090@melville-high.school.nz>
2022-11-02 09:42:02 +1300

    Negative numbers are now broken

--- caesar cipher/simple_caesar_cipher.py
+++ caesar cipher/simple_caesar_cipher.py
@@ -7,19 +7,14 @@ def cipher():
     print(letters)
 
     encode = input("Enter your string to encode: ")
-    key = ''.join(re.findall('[0-9-]', input("How much do you want to shift this by can be a positive or negative integer: ")))
-    while key.__contains__('--'):
-        key = key.replace('--', '-')
+    key = ''.join(re.findall('[0-9]', input("How much do you want to shift this by can be a positive or negative integer: ")))
     key = int(key)
     new_string = []
 
     for letter in encode:
         new_string.append(letters[(letters.index(letter) + key) % len(letters)])
-
     stdout('Output: ' + ''.join(new_string))
 
-# Adds \n and uses the faster version of print
-#review time: 1668091889.6146588
 def stdout(text):
     sys.stdout.write(str(text) + '\n')
 
