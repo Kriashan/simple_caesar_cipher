e1bb67e93caecdbf13a8a8e43a11da09f992d016
cmiles45365346 <cmiles45365346@gmail.com>
2022-11-21 05:00:12 +1300

    stricter sanitization, less bloat, fixed negatives

--- caesar cipher/simple_caesar_cipher.py
+++ caesar cipher/simple_caesar_cipher.py
@@ -2,21 +2,48 @@ import string
 import sys
 import re
 
-def cipher():
-    letters = list(string.ascii_letters + string.digits + string.punctuation + '')
-    print(letters)
+# Encrypt messages
+def encrypt(encrypting):
+    # Available characters
+    letters = list(string.ascii_letters + string.digits + string.punctuation + ' ')
+    
+    # Input
+    encode = input("Enter your string to encrypt: ")
+    
+    # Sanitize
+    key = ''.join(re.findall('[0-9]', input("How much do you want to shift this by must be a whole number: "))) + '0'
+    key = int(key) // 10 # Radix is 10 so this is 0/10 which does nothing and avoids blank error
+    if not encrypting:
+        key = -key
+    encode = ''.join(re.findall('[{}]'.format(''.join(letters)), encode))
 
-    encode = input("Enter your string to encode: ")
-    key = ''.join(re.findall('[0-9]', input("How much do you want to shift this by can be a positive or negative integer: ")))
-    key = int(key)
+    # Encrypt
     new_string = []
-
     for letter in encode:
         new_string.append(letters[(letters.index(letter) + key) % len(letters)])
-    stdout('Output: ' + ''.join(new_string))
+    
+    # stdout output
+    stdout('Output: {}'.format(''.join(new_string)))
 
-def stdout(text):
-    sys.stdout.write(str(text) + '\n')
+# Asks the user if the want to encrypt a message? decrypt a message? or exit the program?
+def cipher():
+    while True:
+        ask = input("Do you want to encrypt a message? or decrypt a message? or exit? ").lower() # lower to avoid capitalization issues
+        if ask == 'encrypt':
+            encrypt(True)
+            break
+        elif ask == 'decrypt':
+            encrypt(False)
+            break
+        elif ask == 'exit':
+            exit(0)
+        else:
+            stdout('Please enter encrypt, or decrypt, or exit')
+
+def stdout(*text):
+    for item in text:
+        sys.stdout.write(str(item) + ' ')
+    sys.stdout.write('\n')
 
 if __name__ == '__main__':
     cipher()
